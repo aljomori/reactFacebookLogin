6343aed27660db3029f96a7d3b81fecca08cd815
Alexis Moreno <amoreno@sosafe.cl>
2019-10-26 14:25:31 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+import * as serviceWorker from './serviceWorker';
+
+const responseFacebook = (response) => {
+    console.log(response);
+}
+
+ReactDOM.render(
+    <FacebookLogin
+        appId="1088597931155576"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+    />,
+    document.getElementById('root')
+);
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
