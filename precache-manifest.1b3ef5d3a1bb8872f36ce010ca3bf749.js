b29610ab745ac8e440c1ee68a92825c9c2f91800
Alexis Moreno <amoreno@sosafe.cl>
2019-11-14 19:45:18 -0300

    Updates

---
+++ precache-manifest.1b3ef5d3a1bb8872f36ce010ca3bf749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306e78abe50eccaf404a5c6138d07d6a",
+    "url": "/login-tester/index.html"
+  },
+  {
+    "revision": "35f9f43fcb9751e04a1d",
+    "url": "/login-tester/static/css/main.63b9a3f7.chunk.css"
+  },
+  {
+    "revision": "0986f21d00118538a540",
+    "url": "/login-tester/static/js/2.5f2eda0f.chunk.js"
+  },
+  {
+    "revision": "35f9f43fcb9751e04a1d",
+    "url": "/login-tester/static/js/main.e7780035.chunk.js"
+  },
+  {
+    "revision": "3afa07b760cf497dabfa",
+    "url": "/login-tester/static/js/runtime-main.aaded999.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/login-tester/static/media/logo.25bf045c.svg"
+  }
+]);
