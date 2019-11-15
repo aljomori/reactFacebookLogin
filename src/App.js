ab12a5813531dde96d84e405a59f61fbe00facd2
Alexis Moreno <amoreno@sosafe.cl>
2019-11-14 21:08:42 -0300

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import FacebookLogin from "react-facebook-login";
+import CopyToClipboard from 'react-copy-to-clipboard';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 import './App.css';
 
-function App() {
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+function App({user, signOut, signInWithGoogle}) {
   const [fb, setFb] = useState({});
+  const [copyFb, setCopyFb] = useState(false);
+  const [copyF, setCopyF] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <FacebookLogin
             appId="414897609429813"
-            autoLoad={true}
+            autoLoad={false}
             fields="name,email,picture"
             callback={(object)=>{setFb(object)}}
             cssClass="my-facebook-button-class"
             icon="fa-facebook"
         />
-        <p style={{maxWidth: '50%', fontSize: '12px'}}>
-          <pre>{JSON.stringify(fb)}</pre>
-        </p>
+        <div style={{maxWidth: '50%', fontSize: '12px', marginBottom: '20px'}}>
+          <CopyToClipboard  className="copy"  text={JSON.stringify(fb)} onCopy={()=>setCopyFb(true)}>
+            <pre>{JSON.stringify(fb)}</pre>
+          </CopyToClipboard>
+          {copyFb?<div>Facebook Copiado</div>:''}
+        </div>
+
+        {
+          user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
+        {
+          user
+              ?<div style={{maxWidth: '50%', fontSize: '12px'}}>
+                <CopyToClipboard className="copy" text={JSON.stringify(user)} onCopy={()=>setCopyF(true)}>
+                  <pre>{JSON.stringify(user)}</pre>
+                </CopyToClipboard>
+                {copyF?<span>Firebase Copiado!</span>:''}
+              </div>
+              : <div><p>Please sign in.</p></div>
+        }
+
       </header>
     </div>
   );
 }
+const firebaseAppAuth = firebaseApp.auth();
 
-export default App;
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAu5Vdumx8Ft3ezwghLDLcn0uffd7MLoBM",
+    authDomain: "tourism-d603b.firebaseapp.com",
+    databaseURL: "https://tourism-d603b.firebaseio.com",
+    projectId: "tourism-d603b",
+    storageBucket: "tourism-d603b.appspot.com",
+    messagingSenderId: "951487490542",
+    appId: "1:951487490542:web:aa0769da62e8752bc446ff",
+    measurementId: "G-VGX1HDP8KC"
+};
+export default config;
